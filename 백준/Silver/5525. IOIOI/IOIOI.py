fdde196b48978cf2bf3796a342ea042089377bfc
ygreee0320 <113974663+ygreee0320@users.noreply.github.com>
2025-02-17 19:46:56 +0900

    [Silver I] Title: IOIOI, Time: 352 ms, Memory: 35344 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -6,9 +6,17 @@ S = sys.stdin.readline().rstrip()
 
 PN = 'IOI' + 'OI' * (N - 1)
 
-count = 0
-for i in range(len(S)):
-    if S[i:i+len(PN)] == PN:
-        count += 1
+result, i, count = 0, 0, 0  # 최종 결과, 찾는 중인 인덱스, 101 연속 개수
 
-print(count)
+while i < (M - 1):
+    if S[i:i+3] == 'IOI':
+        count += 1
+        i += 2
+        if count == N:
+            result += 1
+            count -= 1
+    else:
+        i += 1
+        count = 0
+
+print(result)
