a421135f85d83b158357af6be9bf72772b3e3e83
Gyuri <113974663+ygreee0320@users.noreply.github.com>
2025-11-20 01:35:46 +0900

    [Gold V] Title: 내려가기, Time: 220 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/2096. 내려가기/내려가기.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+arr = list(map(int, input().split()))
+
+max_dp = arr
+min_dp = arr
+
+for _ in range(N - 1):
+    arr = list(map(int, input().split()))
+
+    max_dp = [arr[0] + max(max_dp[0], max_dp[1]), arr[1] + max(max_dp), arr[2] + max(max_dp[1], max_dp[2])]
+    min_dp = [arr[0] + min(min_dp[0], min_dp[1]), arr[1] + min(min_dp), arr[2] + min(min_dp[1], min_dp[2])]
+
+print(max(max_dp), min(min_dp))
