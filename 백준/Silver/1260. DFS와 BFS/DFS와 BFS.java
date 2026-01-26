30829d3c5f205fff365bcb33445e6df1a31f6188
Gyuri <113974663+ygreee0320@users.noreply.github.com>
2026-01-26 17:55:09 +0900

    [Silver II] Title: DFS와 BFS, Time: 196 ms, Memory: 19308 KB -BaekjoonHub

---
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -0,0 +1,73 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M, V;
+    static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        V = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i <= N; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+        }
+
+        for (int i = 1; i <= N; i++) {
+            Collections.sort(graph.get(i));
+        }
+
+        visited = new boolean[N + 1];
+        dfs(V);
+        sb.append("\n");
+
+        visited = new boolean[N + 1];
+        bfs(V);
+
+        System.out.println(sb);
+    }
+
+    static void dfs(int node) {
+        visited[node] = true;
+        sb.append(node).append(" ");
+
+        for (int next: graph.get(node)) {
+            if (!visited[next]) {
+                dfs(next);
+            }
+        }
+    }
+
+    static void bfs(int start) {
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(start);
+        visited[start] = true;
+
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            sb.append(node).append(" ");
+
+            for (int next : graph.get(node)) {
+                if (!visited[next]) {
+                    visited[next] = true;
+                    q.offer(next);
+                }
+            }
+        }
+    }
+}
