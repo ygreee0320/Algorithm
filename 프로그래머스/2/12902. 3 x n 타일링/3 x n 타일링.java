9c1f4a5df1882a2d545d92e400da3cc4ba341d3a
Gyuri <113974663+ygreee0320@users.noreply.github.com>
2026-05-25 23:53:56 +0900

    [level 2] Title: 3 x n 타일링, Time: 15.85 ms, Memory: 60.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12902. 3 x n 타일링/3 x n 타일링.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(int n) {
+        if (n % 2 == 1) return 0;
+        
+        long[] dp = new long[n + 1];
+        dp[0] = 1;
+        dp[2] = 3;
+        
+        for (int i = 4; i <= n; i += 2) {
+            dp[i] = dp[i - 2] * 3;
+            
+            for (int j = i - 4; j >= 0; j -= 2) {
+                dp[i] += dp[j] * 2;
+            }
+            
+            dp[i] %= 1000000007;
+        }
+        
+        return (int) dp[n];
+    }
+}
--- 프로그래머스/2/12902. 3 x n 타일링/README.md
+++ 프로그래머스/2/12902. 3 x n 타일링/README.md
@@ -1,10 +1,10 @@
 # [level 2] 3 x n 타일링 - 12902 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12902) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12902?language=java) 
 
 ### 성능 요약
 
-메모리: 9.27 MB, 시간: 484.27 ms
+메모리: 60.3 MB, 시간: 15.85 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 15일 23:53:55
+2026년 05월 25일 23:53:45
 
 ### 문제 설명
 
