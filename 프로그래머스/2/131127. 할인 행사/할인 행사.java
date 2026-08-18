d92bdef7f4fb1bc58fa21e707feb9900dece54be
Gyuri <113974663+ygreee0320@users.noreply.github.com>
2026-08-18 15:40:45 +0900

    [level 2] Title: 할인 행사, Time: 27.70 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -9,25 +9,21 @@ class Solution {
             map.put(want[i], number[i]);
         }
         
+        // 첫 10일 반영
         for (int i = 0; i < 10; i++) {
             String target = discount[i];
             
             if (map.containsKey(target)) {
                 map.put(target, map.get(target) - 1);
             }
-            
-            boolean possible = true;
-            
-            for (int cnt : map.values()) {
-                if (cnt > 0) possible = false;
-            }
-            
-            if (possible) answer++;
         }
         
+        if (isPossible(map)) answer++;
+        
+        // 가입 시작일을 하루씩 이동
         for (int i = 1; i <= discount.length - 10; i++) {
-            String before = discount[i - 1];
-            String target = discount[i + 9];
+            String before = discount[i - 1]; // 전날 상품
+            String target = discount[i + 9]; // 추가된 상품
             
             if (map.containsKey(before)) {
                 map.put(before, map.get(before) + 1);
@@ -37,15 +33,19 @@ class Solution {
                 map.put(target, map.get(target) - 1);
             }
             
-            boolean possible = true;
-            
-            for (int cnt : map.values()) {
-                if (cnt > 0) possible = false;
-            }
-            
-            if (possible) answer++;
+            if (isPossible(map)) answer++;
         }
         
         return answer;
+    }
+    
+    private boolean isPossible(Map<String, Integer> map) {
+        for (int cnt : map.values()) {
+            if (cnt > 0) {
+                return false;
+            }
+        }
+        
+        return true;
     }
 }
