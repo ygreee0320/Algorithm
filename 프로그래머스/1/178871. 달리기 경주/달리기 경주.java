59792c846a929b909f8c57b7c7f980a231df5d03
Gyuri <113974663+ygreee0320@users.noreply.github.com>
2026-09-17 19:22:18 +0900

    [level 1] Title: 달리기 경주, Time: 208.73 ms, Memory: 375 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
@@ -2,33 +2,23 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String[] players, String[] callings) {
-        int n = players.length;
+        Map<String, Integer> rank = new HashMap<>();
         
-        Map<String, Integer> map_p = new HashMap<>(); // 플레이어 기준
-        Map<Integer, String> map_r = new HashMap<>(); // 랭킹 기준
-        
-        for (int i = 0; i < n; i++) {
-            map_p.put(players[i], i);
-            map_r.put(i, players[i]);
+        for (int i = 0; i < players.length; i++) {
+            rank.put(players[i], i);
         }
         
         for (String runner : callings) {
-            int current = map_p.get(runner);
-            String front_runner = map_r.get(current - 1);
+            int current = rank.get(runner);
+            String frontRunner = players[current - 1];
             
-            map_p.put(runner, current - 1);
-            map_p.put(front_runner, current);
+            players[current - 1] = runner;
+            players[current] = frontRunner;
             
-            map_r.put(current, front_runner);
-            map_r.put(current - 1, runner);
+            rank.put(runner, current - 1);
+            rank.put(frontRunner, current);
         }
         
-        String[] result = new String[n];
-        
-        for (Map.Entry<Integer, String> entry : map_r.entrySet()) {
-            result[entry.getKey()] = entry.getValue();
-        }
-        
-        return result;
+        return players;
     }
 }
